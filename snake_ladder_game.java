1958fbb3d68cb2b1599b3dd61a043880b4f17739
Vishwanath <vishwanathiti828@gmail.com>
2021-12-09 18:23:33 +0530

    added usecase 6

--- snake_ladder_game.java
+++ snake_ladder_game.java
@@ -1,5 +1,5 @@
 public class snake_ladder_game{
-	static int totalPoints, noPlay=0, ladder=1, snake = 2;
+	static int numDice, totalPoints, noPlay=0, ladder=1, snake = 2;
 
 	public static void main(String[] args){
 
@@ -9,10 +9,16 @@ public class snake_ladder_game{
 
 			if ( choice == ladder ){
 				int c = ((totalPoints + dice) > 100) ? (totalPoints += 0): (totalPoints += dice);
+				numDice ++;
+				System.out.println("Total number of times dice rolled is " + numDice + " & Player position is " + totalPoints);
 			}else if ( choice == snake ){
 				int d = ((totalPoints - dice) <= 0) ? (totalPoints = 0): (totalPoints -= dice);
+				numDice ++;
+				System.out.println("Total number of times dice rolled is " + numDice + " & Player position is " + totalPoints);
 			}else {
 				totalPoints += 0;
+				numDice ++;
+				System.out.println("Total number of times dice rolled is " + numDice + " & Player position is " + totalPoints);
 			}
 		}
 		System.out.println("Player has won by Total points of " + totalPoints);

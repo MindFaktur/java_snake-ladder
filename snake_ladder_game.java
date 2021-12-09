23374718f482d86ec4135107464c7fe577753a0d
Vishwanath <vishwanathiti828@gmail.com>
2021-12-09 17:21:10 +0530

    added usecase 2

--- snake_ladder_game.java
+++ snake_ladder_game.java
@@ -1,5 +1,9 @@
 public class snake_ladder_game{
 	public static void main(String[] args){
-		System.out.println("Welcome to snake and ladder game");
+		System.out.println("The number on the dice is " + random_num());
+	}
+
+	public static int random_num(){
+		return (int)((Math.random() * 10 % 6) +1);
 	}
 }

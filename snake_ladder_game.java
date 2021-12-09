a0fcff2d630fadc6e63cc3beb3db7859c4728f5d
Vishwanath <vishwanathiti828@gmail.com>
2021-12-09 18:10:03 +0530

    added usecase 4

--- snake_ladder_game.java
+++ snake_ladder_game.java
@@ -1,16 +1,21 @@
 public class snake_ladder_game{
-	static int noPlay=0, ladder=1, snake = 2;
-	public static void main(String[] args){
-		int choice = random_num(3,0);
-		int dice = random_num(6,1);
+	static int totalPoints, noPlay=0, ladder=1, snake = 2;
 
-		if ( choice == ladder ){
-			System.out.println("The play is ladder, hence the player has moved forward by " + dice + " points");
-		}	else if ( choice == snake ){
-			System.out.println("The play is snake, hence the player has moved back by " + dice + " points");
-		}  else {
-			System.out.println("The play is no play, hence the player has not moved " );
+	public static void main(String[] args){
+
+		while ( totalPoints <= 100 ){
+			int choice = random_num(3,0);
+			int dice = random_num(6,1);
+
+			if ( choice == ladder ){
+				totalPoints += dice;
+			}else if ( choice == snake ){
+				int c = ((totalPoints - dice) <= 0) ? (totalPoints = 0): (totalPoints -= dice);
+			}else {
+				totalPoints += 0;
+			}
 		}
+		System.out.println("Player has won by Total points of " + totalPoints);
 	}
 
 	public static int random_num(int x, int y){
